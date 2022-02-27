c453f2484b7a75653bcdcf378a1efa686ed98a4e
utouto97 <utouto97@gmail.com>
2022-02-27 12:51:01 +0900

    インストール用スクリプトを追加

--- install.sh
+++ install.sh
@@ -2,6 +2,27 @@
 
 DOTPATH=~/.dotfiles
 
+# curl か wget でtarballを取得し展開
+tarball="https://github.com/utouto97/dotfiles/archive/main.tar.gz"
+
+if type "curl" > /dev/null 2>&1; then
+  echo "curl"
+  curl -L "$tarball" | tar zxv
+# elif type "wget" > /dev/null 2>&1; then
+#   echo "wget"
+#   curl -O - "$tarball" | tar zxv
+else
+  die "curl required"
+fi
+
+# DOTPATHへ移動
+mv -f dotfiles-main "$DOTPATH"
+
+cd "$DOTPATH"
+if [ $? -ne 0 ]; then
+  die "not found: $DOTPATH"
+fi
+
 for f in .??*
 do
   [ "$f" == ".git" ] && continue
