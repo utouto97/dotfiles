731c71f6659b203bf88a93e687ae32e18566331e
Yuto <utouto97@gmail.com>
2022-02-28 11:54:16 +0900

    デプロイスクリプトを変更

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-DOTPATH=~/.dotfiles
+# デフォルトは ~/.dotfiles
+: ${DOTPATH:=~/.dotfiles}
 
-# curl か wget でtarballを取得し展開
+# tarball URL
 tarball="https://github.com/utouto97/dotfiles/archive/main.tar.gz"
 
-if type "curl" > /dev/null 2>&1; then
-  echo "curl"
-  curl -L "$tarball" | tar zxv
-# elif type "wget" > /dev/null 2>&1; then
-#   echo "wget"
-#   curl -O - "$tarball" | tar zxv
-else
-  die "curl required"
+# ディレクトリが存在しなければ、curl か wget でtarballを取得し展開
+if [ ! -d $DOTPATH ]; then
+  if type "curl" > /dev/null 2>&1; then
+    echo "curl"
+    curl -L "$tarball" | tar zxv
+  # elif type "wget" > /dev/null 2>&1; then
+  #   echo "wget"
+  #   curl -O - "$tarball" | tar zxv
+  else
+    die "curl required"
+  fi
+
+  # ディレクトリをDOTPATHへ移動
+  mv -f dotfiles-main "$DOTPATH"
 fi
 
-# DOTPATHへ移動
-mv -f dotfiles-main "$DOTPATH"
-
+# DOTPATH に移動
 cd "$DOTPATH"
 if [ $? -ne 0 ]; then
   die "not found: $DOTPATH"
