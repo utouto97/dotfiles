c63f33561ab92d8ca5dcae19b55318ebb360771f
Yuto <utouto97@gmail.com>
2022-02-28 12:02:37 +0900

    deployとinstallの切り分け

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-# デフォルトは ~/.dotfiles
-: ${DOTPATH:=~/.dotfiles}
-
-# tarball URL
-tarball="https://github.com/utouto97/dotfiles/archive/main.tar.gz"
-
-# ディレクトリが存在しなければ、curl か wget でtarballを取得し展開
-if [ ! -d $DOTPATH ]; then
-  if type "curl" > /dev/null 2>&1; then
-    echo "curl"
-    curl -L "$tarball" | tar zxv
-  # elif type "wget" > /dev/null 2>&1; then
-  #   echo "wget"
-  #   curl -O - "$tarball" | tar zxv
-  else
-    die "curl required"
-  fi
-
-  # ディレクトリをDOTPATHへ移動
-  mv -f dotfiles-main "$DOTPATH"
-fi
-
 # DOTPATH に移動
 cd "$DOTPATH"
 if [ $? -ne 0 ]; then
