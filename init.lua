8274c0d81f9a82df979756c744a6964cb8f71b47
utouto97 <utouto97@gmail.com>
2023-08-06 14:04:31 +0900

    add noice.nvim

--- init.lua
+++ init.lua
@@ -43,6 +43,34 @@ require("packer").startup(function(use)
 	-- })
 
 	use({
+		"folke/noice.nvim",
+		requires = {
+			{ "MunifTanjim/nui.nvim" },
+		},
+		wants = { "nivm-notify" },
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	})
+
+	use({
 		"nvim-lualine/lualine.nvim",
 		event = { "InsertEnter", "CursorHold", "FocusLost", "BufRead", "BufNewFile" },
 		wants = { "nvim-web-devicons" },
@@ -567,6 +595,7 @@ require("packer").startup(function(use)
 	use({
 		"rcarriga/nvim-notify",
 		event = { "BufRead", "BufNewFile" },
+		module = { "notify" },
 		config = function()
 			vim.notify = require("notify")
 		end,
