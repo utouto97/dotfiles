a6b8e2d8a35b728fb968609e68c0f467e4ed1ea0
utouto97 <utouto97@gmail.com>
2023-08-06 16:53:31 +0900

    add twi plugins

--- init.lua
+++ init.lua
@@ -22,6 +22,32 @@ require("packer").startup(function(use)
 	--------------------------------------------------
 	-- Appearance
 	--------------------------------------------------
+	use({
+		"lewis6991/hover.nvim",
+		config = function()
+			require("hover").setup({
+				init = function()
+					-- Require providers
+					require("hover.providers.lsp")
+					-- require('hover.providers.gh')
+					-- require('hover.providers.gh_user')
+					-- require('hover.providers.jira')
+					-- require('hover.providers.man')
+					-- require('hover.providers.dictionary')
+				end,
+				preview_opts = {
+					border = nil,
+				},
+				-- Whether the contents of a currently open hover window should be moved
+				-- to a :h preview-window when pressing the hover keymap.
+				preview_window = false,
+				title = true,
+			})
+
+			-- Setup keymaps
+			vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+		end,
+	})
 
 	use({
 		"tanvirtin/monokai.nvim",
@@ -41,6 +67,14 @@ require("packer").startup(function(use)
 	-- 		vim.cmd([[colorscheme tokyonight]])
 	-- 	end,
 	-- })
+
+	use({
+		"mvllow/modes.nvim",
+		tag = "v0.2.0",
+		config = function()
+			require("modes").setup()
+		end,
+	})
 
 	use({
 		"folke/noice.nvim",
@@ -208,7 +242,7 @@ require("packer").startup(function(use)
 				--        set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 				--        set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 				--        set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-				set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+				-- set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 				set("n", "<Leader>n", "<cmd>lua vim.lsp.buf.rename()<CR>")
 				-- set("n", "<Leader>f", "<cmd>lua vim.lsp.buf.formatting()<cr>")
 				vim.api.nvim_create_autocmd({ "CursorHold" }, {
