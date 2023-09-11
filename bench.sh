df80b234fa15204ea9eaa1c7c69b6bae90a6ec73
utouto97 <utouto97@gmail.com>
2023-09-11 17:45:21 +0900

    add bench

--- bench.sh
+++ bench.sh
@@ -13,14 +13,12 @@ esac
 
 $TIME_COMMAND --format="%e" zsh -i -c exit > /dev/null 2>&1
 
-ZSH_STARTUPTIME=$({ for i in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2>&1 | awk '{ total += $1 } END { print total/NR }')
+ZSH_STARTUPTIME=$({ for i in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2>&1 | awk '{ total += $1 } END { print total/NR*1000 }')
 
 cat<<EOJ
-[
-    {
-        "name": "zsh startup time",
-        "unit": "Second",
-        "value": ${ZSH_STARTUPTIME}
-    }
-]
+{
+    "name": "zsh startup time",
+    "unit": "msec",
+    "value": ${ZSH_STARTUPTIME}
+}
 EOJ
