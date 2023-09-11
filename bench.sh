d05eb251c5a00fdaa75ae864c2916a36ac795519
utouto97 <utouto97@gmail.com>
2023-09-11 17:13:03 +0900

    add zsh bench

---
+++ bench.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+os=$(uname -s | tr '[A-Z]' '[a-z]')
+
+case $os in
+    darwin)
+        TIME_COMMAND=gtime
+    ;;
+    linux)
+        TIME_COMMAND=time
+    ;;
+esac
+
+$TIME_COMMAND --format="%e" zsh -i -c exit > /dev/null 2>&1
+
+ZSH_STARTUPTIME=$({ for i in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2>&1 | awk '{ total += $1 } END { print total/NR }')
+
+cat<<EOJ
+[
+    {
+        "name": "zsh startup time",
+        "unit": "Second",
+        "value": ${ZSH_STARTUPTIME}
+    }
+]
+EOJ
