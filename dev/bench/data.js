664c828b9f68ed0edde95689daa7503b3462baa8
github-action-benchmark <github@users.noreply.github.com>
2023-09-11 09:28:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6a303066013707e63ee2817294f9f6b2dcd1666b

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1694424536652,
+  "repoUrl": "https://github.com/utouto97/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "utouto97@gmail.com",
+            "name": "utouto97",
+            "username": "utouto97"
+          },
+          "committer": {
+            "email": "utouto97@gmail.com",
+            "name": "utouto97",
+            "username": "utouto97"
+          },
+          "distinct": true,
+          "id": "6a303066013707e63ee2817294f9f6b2dcd1666b",
+          "message": "add bench",
+          "timestamp": "2023-09-11T18:28:39+09:00",
+          "tree_id": "045a148a810fb71d333c636fb6a8de0886e9c489",
+          "url": "https://github.com/utouto97/dotfiles/commit/6a303066013707e63ee2817294f9f6b2dcd1666b"
+        },
+        "date": 1694424536247,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "vim startup time",
+            "value": 4.4614,
+            "unit": "msec"
+          },
+          {
+            "name": "zsh startup time",
+            "value": 0,
+            "unit": "msec"
+          }
+        ]
+      }
+    ]
+  }
+}
