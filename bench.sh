36426099d3cc89ce3f07e216a45216bc12574c27
utouto97 <utouto97@gmail.com>
2023-09-12 16:36:17 +0900

    fix shellcheck

--- bench.sh
+++ bench.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-os=$(uname -s | tr '[A-Z]' '[a-z]')
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 case $os in
     darwin)
         TIME_COMMAND=gtime
     ;;
     linux)
-        TIME_COMMAND=time
+        TIME_COMMAND="time"
     ;;
 esac
 
 $TIME_COMMAND --format="%e" zsh -i -c exit > /dev/null 2>&1
 
-ZSH_STARTUPTIME=$({ for i in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2>&1 | awk '{ total += $1 } END { print total/NR*1000 }')
+ZSH_STARTUPTIME=$({ for _ in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2>&1 | awk '{ total += $1 } END { print total/NR*1000 }')
 
 cat<<EOJ
 {
