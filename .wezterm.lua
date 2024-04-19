7c930699462f55ffd5d325b8a8c11d728e2928c5
utouto97 <utouto97@gmail.com>
2024-04-19 21:53:36 +0900

    cica font

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.7
-config.font = wezterm.font('HackGen35 Console NF')
+config.font = wezterm.font("Cica")
+-- config.font = wezterm.font('HackGen35 Console NF')
 
 return config
