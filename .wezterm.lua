6e115e9e13958b8a765e0a943e67a23661641418
utouto97 <utouto97@gmail.com>
2024-04-19 20:23:01 +0900

    add wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.7
+config.font = wezterm.font('HackGen35 Console NF')
+
+return config
--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ ln -snf "$HOME/dotfiles/nvim/lua/" "$HOME/.config/nvim/"
 ln -snf "$HOME/dotfiles/sheldon/" "$HOME/.config/sheldon"
 ln -snf "$HOME/dotfiles/starship.toml" "$HOME/.config/starship.toml"
 ln -snf "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -snf "$HOME/dotfiles/.wezterm.lua" "$HOME/.wezterm.lua"
 
